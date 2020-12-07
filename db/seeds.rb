f0b0774e6452122a7193bc8f1a78557e3af64542
kkoshiya <kylekoshiya@gmail.com>
2020-12-06 19:39:18 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -152,7 +152,7 @@ project_11 = Project.create({
 
 project_12 = Project.create({
   user_id: user_4.id,
-  title: "Soy Berry Maker ",
+  title: "Soy Berry Maker",
   body: "One of the freshest creations since the inception of the Sinister Six",
   description: "Pokemon and Marvel what is not to like about this project",
   category: "food",
