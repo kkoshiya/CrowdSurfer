8b05e0806cae9d968b18b071ca2f67dd1c50aae6
kkoshiya <kylekoshiya@gmail.com>
2021-04-15 22:21:35 -0400

    added fetch projects to explore container

--- frontend/components/nav/explore_container.js
+++ frontend/components/nav/explore_container.js
@@ -1,14 +1,16 @@
 import { connect } from 'react-redux';
 import Explore from './explore';
 import {fetchNews} from '../../actions/news_actions';
+import {fetchProjects} from '../../actions/project_actions'
 
 
 const mSTP = (state) => ({
-  news: Object.values(state.entities.news)
+  news: Object.values(state.entities.projects)
 });
 
 const mDTP = (dispatch) => ({
-  fetchNews: () => dispatch(fetchNews())
+  fetchNews: () => dispatch(fetchNews()),
+  fetchProjects: () => dispatch(fetchProjects())
 });
 
 export default connect(mSTP, mDTP)(Explore);
